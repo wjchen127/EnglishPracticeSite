9f9474c73af3c5fdf724818ad10fca5303f7154f
wjchen127 <wjchen127@gmail.com>
2022-12-13 21:03:07 +0800

    新增快捷鍵enter->replay, ctrl+a、ctrl+alt+a->showAnswer

--- components/TestPanel/TypingPanel.tsx
+++ components/TestPanel/TypingPanel.tsx
@@ -29,9 +29,19 @@ const TypingPanel = (props: IMyProps): ReactElement => {
                     currentTarget.value  = currentTarget.dataset.answer
                 }
 
-                //將正確的input顏色變成綠色底線
-                inputsRef.current[index].classList.remove("border-gray-200")
-                inputsRef.current[index].classList.add("border-green-600")
+                if(inputsRef.current[index].dataset.failed){
+                    //將更正正確的input顏色變成黃色底線
+                    inputsRef.current[index].classList.remove("border-gray-200")
+                    inputsRef.current[index].classList.add("border-yellow-400")
+                }else{
+                    //將正確的input顏色變成綠色底線
+                    inputsRef.current[index].classList.remove("border-gray-200")
+                    inputsRef.current[index].classList.add("border-green-600")
+                }
+                
+                //一次答對或訂正過後答對都會標記 dataset.completed = "true"
+                inputsRef.current[index].dataset.completed = "true"
+
                 //一旦輸入正確就跳到下一個input
                 if(inputsRef.current[index+1]){
                     inputsRef.current[index+1].focus()
@@ -39,14 +49,18 @@ const TypingPanel = (props: IMyProps): ReactElement => {
             }else{
                 //輸入錯誤紅色底線
                 inputsRef.current[index].classList.add("border-red-500")
+                //一旦輸入錯誤，標記曾經輸入錯誤過，當再一次輸入正確時，會顯示黃色的底線
+                inputsRef.current[index].dataset.failed = "true"
                 props.replayFunc()
             }
             
         }else{
             //字數介於0以及input長度之間
             //代表答案還沒輸入完
+            inputsRef.current[index].dataset.completed = ""
             inputsRef.current[index].classList.remove("border-green-600")
             inputsRef.current[index].classList.remove("border-red-500")
+            inputsRef.current[index].classList.remove("border-yellow-400")
         }
 
         if(currentTarget.value !== ""){
@@ -69,6 +83,44 @@ const TypingPanel = (props: IMyProps): ReactElement => {
             }
         }else if(e.key === "Backspace" && e.currentTarget.value === ""){
             e.currentTarget.dataset.oldvalue = ""
+        }else if (e.key === "Enter"){
+            props.replayFunc()
+        }else if(e.ctrlKey && e.altKey && e.key === "a"){
+            showAllAnswer()
+        }else if(e.ctrlKey && e.key === "a"){
+            if(e.currentTarget.dataset.index){
+                showSingleAnswer(e.currentTarget.dataset.index)
+            }
+        }
+    }
+
+    function showSingleAnswer(index: string){
+        const i = parseInt(index)
+        const input = inputsRef.current[i]
+        if(input && !input.dataset.completed){
+            if(input.dataset.answer){
+                input.value = input.dataset.answer
+            }
+            input.classList.remove("border-gray-200")
+            input.classList.remove("border-red-500")
+            input.classList.add("border-yellow-400")
+        }
+    }
+
+    function showAllAnswer(){
+        let i = 0
+        while(inputsRef.current[i]){
+            const input = inputsRef.current[i]
+            if(!input.dataset.completed){
+                if(input.dataset.answer){
+                    input.value = input.dataset.answer
+                }
+                input.dataset.completed = "true"
+                input.classList.remove("border-gray-200")
+                input.classList.remove("border-red-500")
+                input.classList.add("border-yellow-400")
+            }
+            i += 1
         }
     }
 
@@ -77,7 +129,7 @@ const TypingPanel = (props: IMyProps): ReactElement => {
             <div className="flex flex-wrap px-2 justify-center">
                 {
                     inputArr ? inputArr.map((word,i)=>{
-                         return <input key={word+i+props.captionIndex} type="text" maxLength={word.length} data-index={i} data-oldvalue={""} data-answer={word} size={word.length}  onChange={(e)=>handleChange(e)} onInput={(e)=>handleOnInput(e)} onKeyUp={(e)=>handleBackSpace(e)}  ref={(ref: HTMLInputElement) => inputsRef.current[i] = ref} className=" text-lg text-center py-0.5 block mx-1 px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0  border-gray-200" />
+                         return <input autoFocus={i===0} key={word+i+props.captionIndex} type="text" maxLength={word.length} data-index={i} data-oldvalue={""} data-answer={word} size={word.length} onChange={(e)=>handleChange(e)} onInput={(e)=>handleOnInput(e)} onKeyUp={(e)=>handleBackSpace(e)}  ref={(ref: HTMLInputElement) => inputsRef.current[i] = ref} className=" text-lg text-center py-0.5 block mx-1 px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0  border-gray-200" />
                     }) : <></>
                 }       
             </div>
