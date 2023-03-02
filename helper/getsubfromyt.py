11e437fdbdfedd8ce983a1b38f0de0b105128b12
Jesse <wjchen127@gmail.com>
2023-03-02 21:17:39 +0800

    修

--- helper/getsubfromyt.py
+++ helper/getsubfromyt.py
@@ -1,4 +1,4 @@
 import sys, json
 from youtube_transcript_api import YouTubeTranscriptApi
-result = YouTubeTranscriptApi.get_transcript(sys.argv[1], languages=['en'])
+result = YouTubeTranscriptApi.get_transcript(sys.argv[1], languages=['en','en-GB'])
 print(json.dumps(result))
--- pages/api/getytcaptions/[youtubeId].ts
+++ pages/api/getytcaptions/[youtubeId].ts
@@ -1,11 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+const path = require('path')
 let { PythonShell } = require('python-shell')
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { youtubeId } = req.query
     let options = {
         mode: 'text',
         args: youtubeId,
-        scriptPath: '/Users/chenweijie/Desktop/englishpracticesite/helper',
+        scriptPath: path.resolve(process.cwd(), "helper"),
     }
     PythonShell.run('getsubfromyt.py', options, function (err: any, results: any[]) {
         if (err) throw err
